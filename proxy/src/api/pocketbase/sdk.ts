f27accf413cbb1c1a61802341cca413743fc958b
André Pedroza <andre@pedroza.dev>
2023-11-21 08:12:22 -0300

    Add QueryClient to window.sdk

--- proxy/src/api/pocketbase/sdk.ts
+++ proxy/src/api/pocketbase/sdk.ts
@@ -1,10 +1,34 @@
 import { UploadRequestError, UploadRequestOption } from "@/types";
 import * as auth from "./auth";
 import { pb } from "./utils";
+import { QueryClient } from "@tanstack/query-core";
+import { persistQueryClientSubscribe } from "@tanstack/query-persist-client-core";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { compress, decompress } from "lz-string";
 
 export const client = pb;
 
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 5 * 60 * 1000 } },
+});
+
+persistQueryClientSubscribe({
+  queryClient,
+  persister: createSyncStoragePersister({
+    storage: window.sessionStorage,
+    key: "SDK_CACHE",
+    serialize: (data) => compress(JSON.stringify(data)),
+    deserialize: (data) => JSON.parse(decompress(data)),
+  }),
+});
+
 const sdk = new Proxy(pb, {
+  get(pb, prop) {
+    if (prop === "qc") {
+      return queryClient;
+    }
+    return pb[prop as keyof typeof pb];
+  },
   set() {
     throw new Error("SDK is immutable");
   },
